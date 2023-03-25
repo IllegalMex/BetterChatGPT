48b608a225463be6eab0536f5a625559560e3150
IllegalMex <99988436+IllegalMex@users.noreply.github.com>
2023-03-25 22:17:51 +0100

    Update AboutMenu.tsx

--- src/components/AboutMenu/AboutMenu.tsx
+++ src/components/AboutMenu/AboutMenu.tsx
@@ -35,7 +35,7 @@ const AboutMenu = () => {
                   ns='about'
                   components={[
                     <a
-                      href='https://github.com/ztjhz/BetterChatGPT'
+                      href='https://github.com/IllegalMex/BetterChatGPT'
                       target='_blank'
                       className='link'
                     />,
@@ -68,7 +68,7 @@ const AboutMenu = () => {
                     ns='about'
                     components={[
                       <a
-                        href='https://github.com/ztjhz/BetterChatGPT'
+                        href='https://github.com/IllegalMex/BetterChatGPT'
                         target='_blank'
                         className='link'
                       />,
